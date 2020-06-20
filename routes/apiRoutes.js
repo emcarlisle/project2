5155a0dee46ee074b0ae90365606f28ff94076c7
Michael Cervantes <mac95860@hotmail.com>
2020-06-20 10:31:46 -0500

    html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,13 @@
 const Router = require('express').Router;
 const postRoutes = require('./posts');
-
+const db = require('../models');
 const apiRoutes = Router();
 
 apiRoutes.use('/posts', postRoutes);
 
-//Create a new user
+//Create a new user then redirect to new profile page
 apiRoutes.post("/signup", (req, res) => {
-
+    //adds all user information into the user table
     db.User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -15,26 +15,43 @@ apiRoutes.post("/signup", (req, res) => {
         username: req.body.username,
         password: req.body.password
     }).then(() => {
-
-        res.redirect(307, "/login");
-
+        req.login(user, function(err) {
+            if (err) {  
+                return next(err);
+            }
+            return res.redirect (`/profiles/${req.user.username}`);
+        })
     });
 });
 
 //Login
+apiRoutes.post('/login', (req, res) => {
+    if (!req.user) {
+        res.json({});
+    }
+    res.json({
+        username: req.user.username,
+        id: req.user.id
+    })
+});
 
+//logout
+apiRoutes.get('/logout', function (req, res) {
+    req.logout();
+    //redirects user to the login page
+    res.redirect("/login");
+})
 
-// apiRoutes.post('/login', passport("local"), (req, res) => {
-//     if (!req.user) {
-//         res.json({});
-//     }
-//     res.json({
-//         username: req.user.email,
-//         id: req.user.id
-//     })
-// });
-
-//
+//delete profile
+apiRoutes.delete('/deleteProfile/:id', function (req, res) {
+    db.User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function(user) {
+        res.json(user)
+    })
+});
 
 module.exports = apiRoutes;
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,6 @@
 const Router = require('express').Router;
 const db = require('../models');
+const path = require("path");
 
 const htmlRoutes = new Router();
 
@@ -31,5 +32,22 @@ htmlRoutes.get('/example/:id', async (req, res) => {
 htmlRoutes.get('*', async (req, res) => {
   res.render('404');
 });
+
+//==================passport html routes=========================================
+//Login Html route
+htmlRoutes.get('/', (req, res) => {
+  if (req.user) {
+    res.redirect("/profile");
+  }
+  sendFile(path.join(_dirname, "/signup"));
+})
+
+htmlRoutes.get('/', (req, res) => {
+  if (req.user) {
+    res.redirect("/profile");
+  }
+  //if user does not exist, send them back to the login page
+  res.sendFile(path.join(_dirname, "../login"));
+})
 
 module.exports = htmlRoutes;
