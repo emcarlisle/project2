a2c195dddd113f0e554daa0a8f859424ace19f24
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-20 17:21:46 -0500

    fixing passport files

--- config/passport.js
+++ config/passport.js
@@ -1,28 +1,49 @@
-const passport = require('passport');
-const LocalStrategy = require("passport-local").Strategy;
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
-const db = require("../models/LogIn");
+var db = require("../models");
 
-//User Authentication
-module.exports = function(passport, db){
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+passport.use(new LocalStrategy(
+  // Our user will sign in using an email, rather than a "username"
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    // When a user tries to sign in this code runs
+    db.User.findOne({
+      where: {
+        email: email
+      }
+    }).then(function(dbUser) {
+      // If there's no user with the given email
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect email."
+        });
+      }
+      // If there is a user with the given email, but the password the user gives us is incorrect
+      else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbUser);
+    });
+  }
+));
 
-    passport.use(new LocalStrategy (
-        function(username, password, done) {
-            db.User.findOne({
-                username: username
-            }, function (err, user) {
-                if (err) {
-                    return done(err);
-                }
+// In order to help keep authentication state across HTTP requests,
+// Sequelize needs to serialize and deserialize the user
+// Just consider this part boilerplate needed to make it all work
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
 
-                if (!user) {
-                    return done(null, false, { message: 'Please give a valid username'});
-                }
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
-                if (!user.validPassword(password)) {
-                    return done(null, false, { message: "hmmm... That password does not match the one we have on record."});
-                }
-            })
-        }
-    ));
-}
+// Exporting our configured passport
+module.exports = passport;
