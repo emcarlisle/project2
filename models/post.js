5e8631072e7101ddbe15af43ff79f875d27d7597
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-21 12:07:00 -0500

    adding association to User

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       },
       content: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
         validate: {
           len: [1]
@@ -28,10 +28,12 @@ module.exports = (sequelize, DataTypes) => {
 
     });
   Post.associate = function (models) {
+    // We're saying that a Post should belong to a User
+    // A Post can't be created without a User due to the foreign key constraint
+
     Post.belongsTo(models.User, {
       foreignKey: {
         allowNull: false,
-        onDelete: "cascade"
       }
     });
     
