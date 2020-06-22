1b192f8f5303c79f4bae293f6a2eeb8fe2d36e80
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-22 14:55:30 -0500

    adjusting post model

--- models/post.js
+++ models/post.js
@@ -3,23 +3,17 @@ module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define(
     'Post',
     {
+      user_id: DataTypes.INTEGER,
       body: {
         type: DataTypes.TEXT,
         allowNull: false,
         validate: {
           len: [1]
         }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
       }
-
+  
     });
-  Post.associate = function (models) {
+    Post.associate = function (models) {
     // We're saying that a Post should belong to a User
     // A Post can't be created without a User due to the foreign key constraint
 
