52900aa06f1075799178961770d645b72db6d205
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-22 14:56:40 -0500

    adding name and adding foreign key for user_id

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
+'use strict'
+
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require('bcryptjs');
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     // The email cannot be null, and must be a proper email before creation
+    name: DataTypes.STRING,
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -33,7 +36,8 @@ module.exports = function(sequelize, DataTypes) {
 
   User.associate = function (models) {
     User.hasMany(models.Post, {
-      onDelete: 'cascade'
+      foreignKey: 'user_id',
+      onDelete: 'CASCADE'
     });
   };
 
