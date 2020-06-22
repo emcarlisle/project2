8b10a814051aeaf6e3684ac46d10a67f813cb3c9
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-22 18:10:45 -0500

    editing migrations

--- migrations/20191212055649-create-example.js
+++ migrations/20191212055649-create-example.js
@@ -8,10 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
+      //name: {
+      //  type: Sequelize.STRING,
+      //  allowNull: false
+      //},
       email: {
         type: Sequelize.STRING,
         allowNull: false,
